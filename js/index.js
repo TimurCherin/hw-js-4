512d9cd2b56075ab80fc478ca7d0911977908d17
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-04-12 19:58:21 +0300

    hw-js-4

---
+++ js/index.js
@@ -0,0 +1,23 @@
+                                                // ex 1
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice)
+                                                // ex 2
+const question = prompt("Скільки вам років");
+const passed = question >= 18;
+console.log(passed);
+                                                // ex 3
+const SAVED_PASSWORD = prompt("Введіть пароль");
+const password = prompt("Введіть пароль повторно");
+const isMath =  SAVED_PASSWORD === password;
+console.log(isMath);
+                                                // ex 4
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
